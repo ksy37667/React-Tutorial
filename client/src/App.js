f06c4156b87907eec20ca98b06daecf9f2892700
LeeHyeongKon <ksy37667@gmail.com>
2020-08-23 10:32:10 +0900

    add get customers rest api module

--- client/src/App.js
+++ client/src/App.js
@@ -9,35 +9,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import { withStyles } from '@material-ui/core/styles';
 
-
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '이형곤',
-    'birthday': '941014',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '홍길동',
-    'birthday': '941014',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '이순신',
-    'birthday': '941014',
-    'gender': '남자',
-    'job': '대학생'
-  }
-]
-
-
 const styles = theme => ({
   root: {
     width: '100%',
@@ -51,6 +22,23 @@ const styles = theme => ({
 
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -67,7 +55,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          { customers.map( c=> { return( <Customer key={c.id} id={c.id}image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+          {this.state.customers ? this.state.customers.map( c=> { return( <Customer key={c.id} id={c.id}image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+          }) : ""}
           </TableBody>
         </Table>
       </Paper>
