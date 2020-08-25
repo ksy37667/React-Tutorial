593a6c1f1a3ea44fe0c3a0baf755f35d6dcf20c3
LeeHyeongKon <ksy37667@gmail.com>
2020-08-25 12:04:23 +0900

    Add Material UI Design for Add/Delete

--- client/src/App.js
+++ client/src/App.js
@@ -14,14 +14,14 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
     minWidth: 1080
   },
   process: {
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing(2),
   }
 })
 
@@ -72,7 +72,7 @@ class App extends Component {
 
   render() {
     const { classes } = this.props;
-    console.log(classes)
+    // console.log(classes)
     // console.log(this.props)
     return (
       <div>
